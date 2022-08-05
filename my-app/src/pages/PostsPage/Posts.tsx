2b6d73f4fb3f239bc4cc69bcff92e27325abb915
Baubek Mazhitbekov <baukenzo16@gmail.com>
2022-08-05 18:23:04 +0600

    testing fetch to Jsonplaceholder

--- my-app/src/pages/PostsPage/Posts.tsx
+++ my-app/src/pages/PostsPage/Posts.tsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios, {AxiosResponse} from 'axios'
 
-interface IPosts {};
+interface IPosts {
+    id: number
+    title: string
+    body: string
+}
 
-export const Posts: React.FC<IPosts> = props => {
+
+
+export const Posts: React.FC<{}> = props => {
+    const [posts, setPosts] = useState<IPosts[]>([])
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/`)
+            .then((response: AxiosResponse) => {
+                setPosts(response.data)
+                console.log('responsing', response.data)
+            })
+    }, [])
     return (
         <div>
             posts page
+
+            {
+                posts.map(item => {
+                    return (
+                        <div>
+                            <p>{item.id}</p>
+                            <p>{item.title}</p>
+                            <p>{item.body}</p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
