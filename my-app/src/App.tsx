089c545a967fa73bb99cda9cb01cf77d96d82cef
Baubek Mazhitbekov <baukenzo16@gmail.com>
2022-08-05 19:02:18 +0600

    adding mui design

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,14 +4,13 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Header} from './components/header/Header';
 import { Posts } from './pages/PostsPage/Posts';
 import { AddPosts } from './pages/AddPostsPage/AddPosts';
+import Appbar from './components/Appbar/Appbar'
 
 function App() {
   return (
     <BrowserRouter>
       <>
-        <div className="App">
-          <Header/>
-        </div>
+         <Appbar/>
         <Routes>
           <Route path='/' element={<Posts/>} />
           <Route path='/addposts' element={<AddPosts/>} />
--- my-app/src/pages/PostsPage/Posts.tsx
+++ my-app/src/pages/PostsPage/Posts.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios, {AxiosResponse} from 'axios'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 interface IPosts {
     id: number
@@ -22,19 +27,31 @@ export const Posts: React.FC<{}> = props => {
     }, [])
     return (
         <div>
-            posts page
-
-            {
-                posts.map(item => {
-                    return (
-                        <div>
-                            <p>{item.id}</p>
-                            <p>{item.title}</p>
-                            <p>{item.body}</p>
-                        </div>
-                    )
-                })
-            }
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    { posts.map(item => {
+                            return (
+                                <Grid item xs={3}>
+                                    <Card sx={{ minWidth: 275 }}>
+                                        <CardContent>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                {item.id}
+                                                </Typography>
+                                                <Typography variant="h5" component="div">
+                                                {item.title}
+                                                </Typography>
+                                                <Typography variant="body2">
+                                                {item.body}
+                                                <br />
+                                                </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            )
+                        })
+                    }
+                </Grid>
+            </Box>
         </div>
     )
 }
